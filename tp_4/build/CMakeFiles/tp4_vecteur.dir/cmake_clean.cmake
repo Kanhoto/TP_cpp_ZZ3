673a1d5bf31a42e41a57b942567f59ba0e89e30b
vicanhotod <vicanhotod@ada.local.isima.fr>
2022-11-07 15:19:10 +0100

    [+]TP_Cpp_ZZ3 - TP4 first questions

---
+++ tp_4/build/CMakeFiles/tp4_vecteur.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp4_vecteur.dir/test/test_vecteur.cpp.o"
+  "tp4_vecteur"
+  "tp4_vecteur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tp4_vecteur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
